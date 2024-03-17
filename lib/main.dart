4dd3aa2db9bec8fa450f982bb532198ab6293ad9
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2024-03-16 23:24:01 -0500

    Dark theme applied to the main widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:gitspace/strings.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Gitspace());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Gitspace extends StatelessWidget {
+  const Gitspace({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gitspace',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+      title: Strings.title,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
         useMaterial3: true,
+        inputDecorationTheme: const InputDecorationTheme(
+          hintStyle: TextStyle(
+            color: Colors.white24,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFFC73966),
+          onPrimary: Colors.white,
+          background: Color(0xFF212429),
+          surface: Color(0xFF26292E),
+          brightness: Brightness.dark,
+        ),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              ),
+            ),
+          ),
+        ),
       ),
-      home: const AddAccountPage(),
+      home: const Scaffold(
+        body: SafeArea(
+          child: AddAccountPage(),
+        ),
+      ),
     );
   }
 }
@@ -25,71 +55,59 @@ class AddAccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 64.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: Text(
-                'Add an account',
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32.0),
-              child: Text(
-                'You can create a personal access token in your profile settings',
-              ),
-            ),
-            const SizedBox(height: 64.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: Text(
-                'Personal access token',
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-            ),
-            const SizedBox(height: 8.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Colors.black12,
-                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                ),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.all(16.0),
-                    hintText: 'Github or Gitlab token',
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 32.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: FilledButton(
-                onPressed: () => {},
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8.0),
-                      ),
-                    ),
-                  ),
-                ),
-                child: const Text('Sign in'),
-              ),
-            )
-          ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const SizedBox(height: 64.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Text(
+            'Add an account',
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
         ),
-      ),
+        const SizedBox(height: 16.0),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 32.0),
+          child: Text(Strings.addAccountPageDescription),
+        ),
+        const SizedBox(height: 64.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Text(
+            Strings.personalAccessToken,
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+        ),
+        const SizedBox(height: 8.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+            ),
+            child: const TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 8.0,
+                ),
+                hintText: Strings.personalAccessTokenHint,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 32.0),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: FilledButton(
+            onPressed: () => {},
+            child: const Text(Strings.signIn),
+          ),
+        )
+      ],
     );
   }
 }
---
+++ lib/strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  static const String title = 'Gitspace';
+  static const String signIn = 'Sign in';
+  static const String addAccountPageTitle = 'Add an account';
+  static const String addAccountPageDescription = 'You can create a personal access token in your profile settings';
+  static const String personalAccessToken = 'Personal access token';
+  static const String personalAccessTokenHint = 'Github or Gitlab token';
+}
